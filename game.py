b4d1f454b84f4b6d4402e320a1b98669d4d59e46
Adam Hale <adam.hale92@googlemail.com>
2024-02-08 10:42:58 +0000

    added player count function

--- game.py
+++ game.py
@@ -3,8 +3,21 @@ import random
 #roll a random number from 1-6 function
 def roll():
     return random.randint(1,6)
-   
-print(roll())
+
+def player_count():
+    while True:
+        number_of_players = input("Choose the number of players (2 - 4):")
+        if number_of_players.isdigit():
+            number_of_players = int(number_of_players)
+            if 2 <= number_of_players <= 4:
+                break
+            print("Error: Number of players needs to be between 2 and 4")
+        else:
+            print("Error: Number of players must be a digit")
+    return number_of_players
+
+players = player_count()
+print(players)
 #choose number of players (2-4)
 #create array of player scores based on number of players
 #if dice rolls 1, 0 score and next person
